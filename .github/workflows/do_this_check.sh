ab2627748a83471964e53e789bb58a0ebdb35a10
John Till <johndtill@gmail.com>
2022-05-01 18:03:28 +0100

    Remove quotes from grep exclude

--- .github/workflows/do_this_check.sh
+++ .github/workflows/do_this_check.sh
@@ -1,4 +1,4 @@
-if grep --exclude=\"*.{sh,txt}" -rnw ${GITHUB_WORKSPACE} -i -e "DO THIS"; then
+if grep --exclude=\*.{sh,txt} -rnw ${GITHUB_WORKSPACE} -i -e "DO THIS"; then
     printf "\nPlease clean up unfinished \"DO THIS\" tasks before merging to main.\n"
     exit 1
 fi
